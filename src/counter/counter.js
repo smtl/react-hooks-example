23902a5980f6169811200f7b8e7fe7376c180c21
ste <rtwste@icloud.com>
2019-05-12 19:33:11 +0100

    React hooks example

---
+++ src/counter/counter.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+      <button onClick={() => setCount(count - 1)}>
+        Decrease
+      </button>
+    </div>
+  );
+}
+
+export default Counter
